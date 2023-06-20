a17027b55ab599856af78c062c0f5697a4921640
kamaathedj <kamaudavikiruku@gmail.com>
2023-06-20 12:24:58 +0300

    added button and useStoreAction hook to simulate theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useStoreActions, Actions, action } from 'easy-peasy';
 import './App.css';
+import ModeStore from './Store/ModeStore';
+import store from './Store/PlayStore';
 
 function App() {
+  const ChangeMode = useStoreActions(
+    (actions: Actions<typeof ModeStore>) => actions.ChangeMode
+  );
+
   return (
     <div>
+      <button type="submit" onClick={ChangeMode}>
+        change Mode
+      </button>
       <h1>hello world</h1>
     </div>
   );
