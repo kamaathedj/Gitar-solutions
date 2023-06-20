b9ec60b819eece8a1b477c73dc4d75ae8a6944c3
kamaathedj <kamaudavikiruku@gmail.com>
2023-06-20 12:23:46 +0300

    added ModeStore to the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import store from './Store/PlayStore';
+import ModeStore from './Store/ModeStore';
+// import store from './Store/PlayStore';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <StoreProvider store={store}>
+    <StoreProvider store={ModeStore}>
       <App />
     </StoreProvider>
   </React.StrictMode>
