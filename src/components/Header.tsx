25e350cf2d9077c902e30e5eadd43d88c413770d
kamaathedj <kamaudavikiruku@gmail.com>
2023-07-01 14:36:43 +0300

    Refactoring css to styled components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,20 +8,36 @@ interface Props {
   children: React.ReactNode;
 }
 const Box = styled.div`
-  display: block,
-  position: fixed,
-  Color:rgb(255, 255, 255),
-  width: 100%,
-  height: 25%,
-  background-color: #5e0472,
+  background: #5e0472;
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 10em;
+`;
+const H3 = styled.h3`
+  color: rgba(97, 218, 251, 0.667);
+  margin-left: 2em;
+  text-align: left;
+`;
+
+const Span = styled.span`
+  text-align: left;
+  margin-left: 2em;
+  border-radius: 10px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: rgba(97, 218, 251, 0.667);
 `;
 
 function Header({ name, children, count }: Props) {
   return (
     <Box>
-      <h3>{name}</h3>
+      <H3>{name}</H3>
       <img src={logo} alt="logo" className="logotext" />
-      {count > 0 ? <span className="logotext count">{count}</span> : <p />}
+      {count > 0 ? <Span>{count}</Span> : <p />}
       <NavLinkRouter />
     </Box>
   );
