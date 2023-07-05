1761e30b2157f1ff6422f5010c7498d2382c06a5
kamaathedj <kamaudavikiruku@gmail.com>
2023-07-05 12:13:51 +0300

    minor fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import { MouseEventHandler } from 'react';
 import { useStoreActions, useStoreState } from '../hooks/Easy-peasy-hooks';
 import Header from '../components/Header';
 
 function Home() {
   const { count, data } = useStoreState((state) => state.products);
   const { ChangeMode } = useStoreActions((state) => state.theme);
+  const handleClick = (e: MouseEvent) => {
+    e.preventDefault();
+    ChangeMode(undefined);
+  };
   return (
     <>
       <Header name="Gitar solutions" count={count}>
@@ -17,7 +22,7 @@ function Home() {
           </>
         ))}
       </ul>
-      <button type="submit" onClick={ChangeMode}>
+      <button type="submit" onClick={handleClick}>
         {' '}
         change to mode
       </button>
